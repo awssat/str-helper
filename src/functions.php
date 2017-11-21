6f74c3732743deeebacfbd8db41cb58d7b4158d8
abdumu <saanina@gmail.com>
2017-11-21 04:01:10 +0000

    Apply fixes from StyleCI

--- src/functions.php
+++ src/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * A flexible & powerful string manipulation helper for Laravel
+ * A flexible & powerful string manipulation helper for Laravel.
  *
  *  @author abdumu <hi@abdumu.com>
  *
@@ -33,11 +33,11 @@ function str($value = null)
             if ($this->falseIfTriggered || $this->falseElseTriggered) {
                 return $this;
             }
-            
 
             // in case of if{method}()
             if (substr($methodName, 0, 2) === 'if') {
                 $methodName = ltrim(substr($methodName, 2), '_');
+
                 return $this->if(function ($v) use ($methodName, $arguments) {
                     return $this->$methodName(...$arguments);
                 });
@@ -50,7 +50,7 @@ function str($value = null)
                 } else {
                     array_unshift($arguments, $this->currentString);
                 }
-                
+
                 $result = call_user_func_array('Illuminate\Support\Str::'.$methodName, $arguments);
             }
 
@@ -58,7 +58,7 @@ function str($value = null)
             elseif (function_exists(str($methodName)->snake()->get())) {
                 return $this->do(str($methodName)->snake()->get(), ...$arguments);
             }
-            
+
             // Couldn't find either?
             else {
                 throw new \Exception('Method ('.$methodName.') is not a valid Illuminate\Support\Str method!');
@@ -111,35 +111,35 @@ function str($value = null)
             if ($this->falseIfTriggered || $this->falseElseTriggered) {
                 return $this;
             }
-            
+
             //anonymose
             if ($callable instanceof Closure) {
                 $result = $callable($this->currentString);
             }
             //regular functions
             elseif (function_exists($callable)) {
-                
+
                 //If a regular function is called, then we get its information
                 //and where is the position of the string value among the params
                 $functionInfo = new ReflectionFunction($callable);
-                
+
                 if ($functionInfo->isDeprecated() || $functionInfo->isDisabled()) {
                     throw new \Exception('Method ('.$callable.') is disabled or deprecated!');
                 }
-                
+
                 if ($functionInfo->getNumberOfParameters() > 1) {
                     $stringIndex = 0;
-                    
+
                     foreach ($functionInfo->getParameters() as $order=>$arg) {
                         if (in_array($arg->name, ['str', 'string', 'subject', 'haystack'])) {
                             $stringIndex = $order;
                             break;
                         }
                     }
-                    
+
                     //add the string param in the right order
                     array_splice($args, $stringIndex, 0, [$this->currentString]);
-                    
+
                     $result = $callable(...$args);
                 } elseif ($functionInfo->getNumberOfParameters() == 1) {
                     $result = $callable($this->currentString);
@@ -158,21 +158,21 @@ function str($value = null)
 
             return $this;
         }
-        
+
         /**
-         * If condition, to end the condition use endif()
+         * If condition, to end the condition use endif().
          *
          * @param callable $callable anonymous function, or name of a function
-         * @param mixed $args
+         * @param mixed    $args
          *
          * @return self
          */
         public function if($callable, ...$args)
         {
             $lastString = $this->currentString;
-    
+
             $result = $this->do($callable, ...$args);
-            
+
             if ($result instanceof self && strcmp($lastString, $this->currentString) === 0) {
                 $this->falseIfTriggered = true;
             } elseif ($result instanceof Traversable && count($result) == 0) {
@@ -180,39 +180,41 @@ function str($value = null)
             } elseif ($result === false) {
                 $this->falseIfTriggered = true;
             }
-    
+
             return $this;
         }
-        
+
         /**
-         * End the If condition
+         * End the If condition.
+         *
          * @return self
          */
         public function endif()
         {
             $this->falseIfTriggered = false;
             $this->falseElseTriggered = false;
-            
-            return $this;
-        }
-        
-        /**
-         * else case of an initiated condition
-         * @return self
-         */
-        public function else()
-        {
-            if (! $this->falseIfTriggered) {
-                $this->falseElseTriggered = true;
-            }
-            
-            $this->falseIfTriggered = false;
-            
+
             return $this;
         }
 
         /**
-         * get the current string
+         * else case of an initiated condition.
+         *
+         * @return self
+         */
+        public function else()
+        {
+            if (!$this->falseIfTriggered) {
+                $this->falseElseTriggered = true;
+            }
+
+            $this->falseIfTriggered = false;
+
+            return $this;
+        }
+
+        /**
+         * get the current string.
          *
          * @return string
          */
